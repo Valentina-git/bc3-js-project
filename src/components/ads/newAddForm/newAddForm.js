b098196a19f85b4cf3fbde636dafacc2e58c939a
Valentina <dert12345f@ukr.net>
2020-12-23 11:50:53 +0200

    img problem

--- src/components/ads/newAddForm/newAddForm.js
+++ src/components/ads/newAddForm/newAddForm.js
@@ -1,46 +1,91 @@
 import './newAddForm.css';
 import validator from 'validator';
 import axios from 'axios';
-import newAddForm from './newAddForm.hbs';
-import { modalBackDrop } from '../../../components/modal/modalBackDrop'
-console.log(modalBackDrop);
-
-window.addEventListener('DOMContentLoaded', () => {
-    let btn = document.querySelector('.modal-btn');
-    let modal = document.querySelector('.modal');
-    // modal.style.display = 'none';
-    let x = document.querySelector('.close');
-
-    console.log(btn);
-    console.log(modal);
-    console.log(x);
+import newAddForm from '../newAddForm/templates/newAddForm.hbs';
+import inputPlus from '../newAddForm/templates/inputPlus.hbs'
+import inputGrey from '../newAddForm/templates/inputGrey.hbs'
+import { modalBackDrop } from '../../modal/modalBackDrop';
 
 
-    btn.addEventListener('click', ( ) => {
-        modal.style.display = 'block';
-    }) 
+const createModalMarkup = document.querySelector('.markup_btn')
+createModalMarkup.innerHTML = newAddForm();
+
+const formAdv = document.forms.newAdvForm;
+const baseURL = 'https://callboard-backend.herokuapp.com/call';
+
+const newObjAdv = {
+    title: '',
+    description: '',
+    category: '',
+    price: '',
+    phone: '',
+    file: [],
+}
+
+const toDataURL = (elem) => {
+    return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onLoadend = () => resolve(reader.result)
+        reader.readAsDataURL(elem.files[0])
+    })
+}
+
+const createBox = () => {
+    const inputWrapper = document.querySelector('.input_wrapper');
+    inputWrapper.innerHTML = '';
+    console.log(newObjAdv);
     
-    btn.addEventListener('mouseover', ( ) => {
-        btn.style.cursor = 'pointer';
-    })
+    newObjAdv
+        .file
+        .forEach(img => inputWrapper
+            .insertAdjacentHTML('beforeend',
+                `<img src=${img} alt="" class ="img-adv-box" name="file">`))
+    if (newObjAdv.file.length < 6) {
+        inputWrapper
+            .insertAdjacentHTML('beforeend', inputPlus())
+        const input = document.querySelector('.input_load_file')
+        console.log(input);
 
-    x.addEventListener('click', ( ) => {
-        modal.style.display = 'none';
+        input.addEventListener('change', async (event) => {
+            console.log('hello');
+
+             toDataURL(event.target)
+                .then(data => {
+                    console.log(data)
+                    newObjAdv.file.push(data)
+                }).then(createBox)
+            console.log(newObjAdv);
+            })
+    }
+    for (let i = newObjAdv.file.length; i < 5; i += 1) {
+        inputWrapper
+            .insertAdjacentHTML('beforeend', inputGrey())
+    }
+}
+createBox()
+
+const getFormData = (event) => {
+    if (event.target === 'file') {
+        return
+    }
+    const { name, value } = event.target;
+    newObjAdv[name] = value;
+}
+formAdv.addEventListener('input', getFormData)
+
+const postNewAdv = async (event) => {
+    event.preventDefault();
     
+    const result = await axios.post(baseURL, {
+        ...newObjAdv, price: Number(newObjAdv.price)
     })
-    x.addEventListener('mouseover', ( ) => { 
-        x.style.cursor = 'pointer';
-    })
+}
+formAdv.addEventListener('submit', postNewAdv)
 
-    window.addEventListener('click', (event) => {
-        if (event.target === modal) {
-            modal.style.display = 'none';
-        }
-    })
 
-    window.addEventListener('keydown', (event) => {
-         if (event.key === 'Escape'){
-            modal.style.display = 'none';
-         }
-        })
-})
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import '../node_modules/normalize.css/normalize.css';
 import './styles.css';
-import './components/header/js/header';
-import './components/footer/styles.js';
-import './components/card/index';
-import './components/hero/slider-item/slider-item';
-import './components/loader/index';
+// import './components/header/js/header';
+// import './components/footer/styles.js';
+// import './components/card/index';
+// import './components/hero/slider-item/slider-item';
+// import './components/loader/index';
 // import './components/auth/auth'
-import './components/modal/modalBackDrop.js';
-import './components/ads/newAddForm/newAddForm';
+// import './components/modal/modalBackDrop.js';
+import './components/ads/newAddForm/newAddForm.js';
